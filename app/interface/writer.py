f8c387bf94c6ec36f80d5f8fe6f9cd9698c79bdf
Juliana Gutierrez <alejuli.0627@gmail.com>
2024-05-29 10:12:38 -0500

    refactor(first_part_project): Simplify the blocks of code from the first part of the project (Part 1)

--- app/interface/writer.py
+++ app/interface/writer.py
@@ -1,3 +1,28 @@
 # Classes for read/write an image on host computer
-# Classes for read/write an Audio file on host computer
 # Classes for read/write an text file on host computer
+# this code will come from making the wave example.py file into a class and the plot audio.py file
+class AudioData:
+    def save_metadata(self):
+        # wave example.py
+        pass
+
+    def save_wave_form(self):
+        # plot audio.py
+        pass
+# Classes for read/write an Audio file on host computer
+# this come from load mp3.py and the record mic.py
+
+
+class AudioStream:
+
+    def to_mp3(self):
+        # Code from load_mp3
+        pass
+
+    def stream_from_source(self):
+        # record mic.py
+        pass
+
+    def save_to_file(self):
+        pass
+
--- first_part_project/load_mp3.py
+++ first_part_project/load_mp3.py
@@ -3,9 +3,9 @@ from pydub import AudioSegment
 
 audio = AudioSegment.from_wav("output.wav")
 
-audio = audio + 6
-audio = audio * 2
-audio = audio.fade_in(2000)
+audio = audio + 6  # this will increase the volume by 6dB
+audio = audio * 2  # repeat the clips
+audio = audio.fade_in(2000)  # The fade_in method is used to gradually increase the volume of the audio over a specified duration (2000 milliseconds).
 
 audio.export("mashup.mp3", format="mp3")
 
