7da2c7c48c22ca400a18a00df40b37fa0d28f2bd
Juliana Gutierrez <alejuli.0627@gmail.com>
2024-06-19 15:30:49 -0500

    chore: rename variables

--- first_part_project/record_mic.py
+++ first_part_project/record_mic.py
@@ -1,50 +1,45 @@
 import pyaudio
 import wave
 
-FRAMES_PER_BUFFER = 3200
-FORMAT = pyaudio.paInt16
-CHANNELS = 1
-RATE = 44100  # We can use 16000 for a change
-p = pyaudio.PyAudio()
 
 # typehints en todas las funciones
-# Poner los segundos como parametro de la primera funcion 
-def record_audio(p) -> pyaudio.PyAudio:
-    
-    #Usar contextmanager
-    stream = p.open(
-        format=FORMAT,
-        channels=CHANNELS,
-        rate=RATE,
+# Poner los segundos como parametro de la primera funcion
+def record_audio(
+    pyaudio_object:pyaudio.PyAudio, rate:int, channels:int, format:int, frames_per_buffer:int, seconds:int
+) -> pyaudio.PyAudio:
+
+    # Usar contextmanager
+    stream = pyaudio_object.open(
+        format=format,
+        channels=channels,
+        rate=rate,
         input=True,
-        frames_per_buffer=FRAMES_PER_BUFFER
+        frames_per_buffer=frames_per_buffer,
     )
 
     print("Start recording")
 
-    seconds = 6
+
     frames = []
-    for i in range(0, int(RATE/FRAMES_PER_BUFFER*seconds)):
-        data = stream.read(FRAMES_PER_BUFFER)
+    for i in range(0, int(rate / frames_per_buffer * seconds)):
+        data = stream.read(frames_per_buffer)
         frames.append(data)
 
     stream.stop_stream()
     stream.close()
-    p.terminate()
+    pyaudio_object.terminate()
 
     return frames
 
 
-def save_audio(frames):
+def save_audio(frames, channels, pyaudio_object, rate):
     obj = wave.open("output.wav", "wb")
-    obj.setnchannels(CHANNELS)
-    obj.setsampwidth(p.get_sample_size(FORMAT))
-    obj.setframerate(RATE)
+    obj.setnchannels(channels)
+    obj.setsampwidth(pyaudio_object.get_sample_size(format))
+    obj.setframerate(rate)
     obj.writeframes(b"".join(frames))
     obj.close()
 
     return obj
 
 
-frames = record_audio(p)
-save_audio(frames)
