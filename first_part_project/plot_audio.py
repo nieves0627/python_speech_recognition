9a5a3fc018f1e6fbf0043e54c196dfe0cab7ed53
Juliana Gutierrez <alejuli.0627@gmail.com>
2024-06-19 15:29:13 -0500

    chore: rename variables

--- first_part_project/plot_audio.py
+++ first_part_project/plot_audio.py
@@ -2,28 +2,27 @@ import wave
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 def get_audio_params(input_path):
     # Cambie wave. open a utilizar context managers
     obj = wave.open(input_path, "rb")
 
-    sample_freq = obj.getframerate()
-    n_samples = obj.getnframes()
-    signal_wave = obj.readframes(-1)
+    framerate = obj.getframerate()
+    nframes = obj.getnframes()
+    frames = obj.readframes(-1)
 
     obj.close()
     #Wave.open
 
-    time_audio = n_samples / sample_freq
+    time_audio = nframes/ framerate
 
     # Retorne un diccionario con esos datos
-    return n_samples, signal_wave, time_audio
+    return nframes, frames, time_audio
 
 
 # Uno de los parametros de la funcion debe ser el output_path donde se guardara la imagen
-def audio_plot(n_samples, signal_wave, time_audio):
-    signal_array = np.frombuffer(signal_wave, dtype=np.int16)  # Axis y. Each sample is 2 bytes long
-    times = np.linspace(0, time_audio, num=n_samples)  # Axis x.
+def audio_plot(nframes, frames, time_audio):
+    signal_array = np.frombuffer(frames, dtype=np.int16)  # Axis y. Each sample is 2 bytes long
+    times = np.linspace(0, time_audio, num=nframes)  # Axis x.
 
     plt.figure(figsize=(15, 5))
     plt.plot(times, signal_array)
